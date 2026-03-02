8adee1f6c6cb6dc713f48322b61ab05f9cb59b62
YannTheMan03 <yann.bolton@outlook.com>
2026-03-02 14:26:44 +0000

    Changes

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 document.getElementById("container").addEventListener("mouseenter", enteredContainer);
 document.getElementById("container").addEventListener('mouseleave', leftContainer);
+
+/* testing things
+const x = document.querySelector(".container");
+function displayCard(){
+    x.style.opacity="0";
+} */
+
+
 const cardVariable = document.querySelectorAll(".card");
 function enteredContainer(){
     console.log("Entered container");
-    cardVariable.forEach( card => card.style.opacity="1");
+    cardVariable.forEach( Element => Element.style.opacity="1");
+    //cardVariable.forEach(displayCard);
 }
 function leftContainer(){
     console.log("Left container");
